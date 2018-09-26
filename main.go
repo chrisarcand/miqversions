2693d93c425a2ba2519a982855a9c8143710c074
Joe Rafaniello <jrafanie@redhat.com>
2018-09-26 12:07:58 -0400

    Add ruby and postgresql versions for each release

--- main.go
+++ main.go
@@ -8,20 +8,20 @@ import (
 
 func main() {
 	data := [][]string{
-		[]string{"", "5.1.z", "2.0"},
-		[]string{"", "5.2.z", "3.0"},
-		[]string{"Anand", "5.3.z", "3.1"},
-		[]string{"Botvinnik", "5.4.z", "3.2"},
-		[]string{"Capablanca", "5.5.z", "4.0"},
-		[]string{"Darga", "5.6.z", "4.1"},
-		[]string{"Euwe", "5.7.z", "4.2"},
-		[]string{"Fine", "5.8.z", "4.5"},
-		[]string{"Gaprindashvili", "5.9.z", "4.6"},
-		[]string{"Hammer", "5.10.z", "4.7"},
+		[]string{"", "5.1.z", "2.0", "", ""},
+		[]string{"", "5.2.z", "3.0", "", ""},
+		[]string{"Anand", "5.3.z", "3.1", "", ""},
+		[]string{"Botvinnik", "5.4.z", "3.2", "", ""},
+		[]string{"Capablanca", "5.5.z", "4.0", "2.2.x", "9.4.x"},
+		[]string{"Darga", "5.6.z", "4.1", "2.2.x", "9.4.x"},
+		[]string{"Euwe", "5.7.z", "4.2", "2.3.x", "9.5.x"},
+		[]string{"Fine", "5.8.z", "4.5", "2.3.x", "9.5.x"},
+		[]string{"Gaprindashvili", "5.9.z", "4.6", "2.3.x", "9.5.x"},
+		[]string{"Hammer", "5.10.z", "4.7", "2.4.x", "9.5.x"},
 	}
 
 	table := tablewriter.NewWriter(os.Stdout)
-	table.SetHeader([]string{"ManageIQ", "CloudForms Management Engine", "CloudForms"})
+	table.SetHeader([]string{"ManageIQ", "CloudForms Management Engine", "CloudForms", "Ruby", "PostgreSQL"})
 
 	for _, v := range data {
 		table.Append(v)
