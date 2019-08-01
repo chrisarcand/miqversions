5abb212a623d6c6b6233d8e15fd759966c4f683a
Joe Rafaniello <jrafanie@redhat.com>
2019-08-01 16:52:15 -0400

    .z for patch, .y for minor releases

--- main.go
+++ main.go
@@ -12,13 +12,13 @@ func main() {
 		[]string{"", "5.2.z", "3.0", "", ""},
 		[]string{"Anand", "5.3.z", "3.1", "", ""},
 		[]string{"Botvinnik", "5.4.z", "3.2", "", ""},
-		[]string{"Capablanca", "5.5.z", "4.0", "2.2.x", "9.4.x"},
-		[]string{"Darga", "5.6.z", "4.1", "2.2.x", "9.4.x"},
-		[]string{"Euwe", "5.7.z", "4.2", "2.3.x", "9.5.x"},
-		[]string{"Fine", "5.8.z", "4.5", "2.3.x", "9.5.x"},
-		[]string{"Gaprindashvili", "5.9.z", "4.6", "2.3.x", "9.5.x"},
-		[]string{"Hammer", "5.10.z", "4.7", "2.4.x", "9.5.x"},
-		[]string{"Ivanchuk", "5.11.z", "5.0", "2.5.x", "10.x"},
+		[]string{"Capablanca", "5.5.z", "4.0", "2.2.z", "9.4.z"},
+		[]string{"Darga", "5.6.z", "4.1", "2.2.z", "9.4.z"},
+		[]string{"Euwe", "5.7.z", "4.2", "2.3.z", "9.5.z"},
+		[]string{"Fine", "5.8.z", "4.5", "2.3.z", "9.5.z"},
+		[]string{"Gaprindashvili", "5.9.z", "4.6", "2.3.z", "9.5.z"},
+		[]string{"Hammer", "5.10.z", "4.7", "2.4.z", "9.5.z"},
+		[]string{"Ivanchuk", "5.11.z", "5.0", "2.5.z", "10.y"},
 	}
 
 	table := tablewriter.NewWriter(os.Stdout)
